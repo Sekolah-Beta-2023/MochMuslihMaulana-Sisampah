bb54ded8cb37b2117f73010c9ca06fd18d46dea2
muslihm3 <muslihmaulanam3@gmail.com>
2023-08-26 15:14:52 +0700

    first commit

---
+++ components/TheFooter.vue
@@ -0,0 +1,110 @@
+<template>
+  <!-- Footer -->
+  <footer class="text-center text-lg-start bg-light text-muted">
+    <div class="container">
+      <!-- Section: Social media -->
+      <section
+        class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+      >
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+          <span
+            >Berikut profil singkat dan informasi sosial media pembuat situs web
+            ini:</span
+          >
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+          <a href="facebook.com/muslih3m" class="me-4 text-reset">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="twitter.com/muslih_m3" class="me-4 text-reset">
+            <i class="fab fa-twitter"></i>
+          </a>
+
+          <a href="instagram.com/muslih_m3" class="me-4 text-reset">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="linkedin.com/in/muslihm3" class="me-4 text-reset">
+            <i class="fab fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/muslihm3" class="me-4 text-reset">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+        <!-- Right -->
+      </section>
+      <!-- Section: Social media -->
+
+      <!-- Section: Links  -->
+      <section class="">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <!-- Content -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                <i class="fas fa-gem me-3"></i>sisampah
+              </h6>
+              <p>
+                sisampah memiliki tujuan utama untuk mengurangi dampak negatif
+                sampah terhadap lingkungan, mendorong daur ulang dan penggunaan
+                kembali bahan-bahan, serta mengurangi volume sampah yang
+                mencemari. Situs ini di buat oleh Moch Muslih Maulana asal dari
+                Kota Batang, Jawa Tengah
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Menu</h6>
+              <p>
+                <a href="#!" class="text-reset">Home</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">About</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Contact</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+              <p><i class="fas fa-home me-3"></i> Batang, Jawa Tengah</p>
+              <p>
+                <i class="fas fa-envelope me-3"></i>
+                muslihmaulanam3@gmail.com
+              </p>
+              <p><i class="fas fa-phone me-3"></i> +628818669227</p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div
+        class="text-center p-4"
+        style="background-color: rgba(0, 0, 0, 0.05)"
+      >
+        © 2023 Copyright:
+        <a class="text-reset fw-bold" href="https://wa.me/628818669227"
+          >sisampah</a
+        >
+      </div>
+      <!-- Copyright -->
+    </div>
+  </footer>
+  <!-- Footer -->
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="py-4">
+    <div class="container">
+      <div class="title border-bottom">
+        <h5>Setor Sampah</h5>
+      </div>
+      <div class="list-sampah">
+        <div
+          class="item-sampah d-flex align-items-center border-bottom pt-3 pb-4"
+        >
+          <input id="sampah" type="checkbox" name="status" class="me-2" />
+          <div class="title-sampah">Sampah Organik</div>
+        </div>
+      </div>
+      <div class="action py-2">
+        <button type="submit" class="btn btn-primary btn-block">Setor</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style></style>
+
+<!-- <template>
+  <div id="app">
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// Memanggil component sesuai lokasi masing - masing file
+import Header from '@/components/TheHeader.vue'
+import Footer from '@/components/TheFooter.vue'
+export default {
+  // Dideklarasikan agar dapat diakses oleh template
+  components: {
+    Header,
+    Footer,
+  },
+}
+</script>
+<style></style> -->
